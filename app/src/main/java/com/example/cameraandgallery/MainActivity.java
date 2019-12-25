d350c4709c8257f0714111896bb5a78a326ec5e5
prokash10 <prokasnjr10@gmail.com>
2019-12-25 11:49:38 +0545

    qwer

--- app/src/main/java/com/example/cameraandgallery/MainActivity.java
+++ app/src/main/java/com/example/cameraandgallery/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.cameraandgallery;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -7,6 +8,7 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.widget.Button;
@@ -23,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         btnc=findViewById(R.id.btnc);
         ivpp=findViewById(R.id.ivpp);
         checkPermission();
+        loadcamera();
 
 
     }
@@ -34,6 +37,20 @@ public class MainActivity extends AppCompatActivity {
                     {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
         }
     }
+
+
+
+    @Override
+    public void onActivityReenter(int resultCode, Intent data) {
+        super.onActivityReenter(resultCode, data);
+        if (resultCode==0 && resultCode==RESULT_OK)
+        {
+            Bundle extras=data.getExtras();
+            Bitmap imageBitmap=(Bitmap) extras.get("data");
+            ivpp.setImageBitmap(imageBitmap);
+        }
+    }
+
     private void loadcamera()
     {
         Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
